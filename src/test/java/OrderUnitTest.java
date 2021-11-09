82c6ef2b70382c0ed29fb35ef62e696f8a5c5802
GeikenPack <jomijo6936@gmail.com>
2021-11-09 17:10:58 +0100

    Fin du tp

---
+++ src/test/java/OrderUnitTest.java
@@ -0,0 +1,17 @@
+package test.java;
+
+import org.junit.Test;
+import java.util.List;
+import static org.junit.Assert.assertEquals;
+import main.java.code.Order;
+
+public class OrderUnitTest {
+
+    @Test public void empty_order_by_default(){
+        Order o = new Order();
+        o.setOwner("Romeo");
+        o.setRecipient("Juliet");
+        List<Order.Drink> drinks = o.getDrinks();
+        assertEquals(0, drinks.size());
+    }
+}
